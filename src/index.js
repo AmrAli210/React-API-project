2c90bd0f3c2586dd964fd187eb363f4bc1d6354f
Amr Ali <amr40267@gmail.com>
2023-04-23 00:22:26 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
